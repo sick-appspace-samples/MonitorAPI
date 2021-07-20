ed91e6d7c07f95d986169fc1053e36b7a4baa1f3
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:40:27 +0200

    'Update'

--- MonitorAPI/scripts/monitorAPI.lua
+++ MonitorAPI/scripts/monitorAPI.lua
@@ -1,28 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  MonitorAPI
-
-  Summary:
-  Introduction to device and App monitoring
-
-  Description:
-  This Sample shows how to monitor several aspects of the device and the currently
-  installed Apps. The data is retrieved cyclical based on timer expiration.
-
-  The following aspects will be monitored:
-  - CPU load
-  - Network load
-  - AppData usage
-  - Memory usage
-  - App status
-
-  How to run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Output is printed to the console.
-  To run this sample, a device with AppEngine >= 2.6.0 is required.
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Check if Monitor API is available
